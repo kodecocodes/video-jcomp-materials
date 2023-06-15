561ad7ced93fc8bb157a84c897aa3e8e8beeb843
Ian Okumu <okumu.otsembo@gmail.com>
2023-06-15 16:49:37 +0300

    Add episode 10 starter and final code

---
+++ 10-challenge-add-a-textfeld/final/app/src/main/java/com/kodeco/android/yummyapp/ui/components/ProfileBar.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2023 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * This project and source code may use libraries or frameworks that are
+ * released under various Open-Source licenses. Use of those libraries and
+ * frameworks are governed by their own individual licenses.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.kodeco.android.yummyapp.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.kodeco.android.yummyapp.R
+
+@Composable
+fun ProfileBar(){
+  Row(
+      modifier = Modifier
+          .fillMaxWidth()
+          .padding(16.dp)) {
+
+    Image(
+        painter = painterResource(id = R.drawable.ic_profile),
+        contentDescription = null,
+        modifier = Modifier.size(50.dp))
+
+    Column(
+        modifier = Modifier
+            .width(200.dp)
+            .height(100.dp)
+            .padding(10.dp),
+        verticalArrangement = Arrangement.SpaceBetween) {
+
+      Text(
+          text = "Hello, Kodeco",
+          style = MaterialTheme.typography.bodyLarge)
+
+    }
+
+
+
+    Box(
+        modifier = Modifier
+            .background(color = Color.White, shape = CircleShape)
+            .size(50.dp)
+            .clip(CircleShape),
+        contentAlignment = Alignment.Center) {
+
+      Image(
+          painter = painterResource(id = R.drawable.ic_notifications),
+          contentDescription = null)
+
+    }
+
+
+
+  }
+}
